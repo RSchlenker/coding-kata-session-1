0130e2335d814c815fb69c0c81f513de7004ca2b
Philipp Krauss <philipp.krauss@codecentric.de>
2022-07-15 11:31:27 +0200

    Implement kata

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+//functional style
 import * as R from 'ramda'
+const isDivisibleBy = R.curry(R.pipe(R.modulo, R.equals(0)))
+const isDivisibleBy400 = isDivisibleBy(R.__, 400)
+const notIsDivisibleBy100 = R.pipe(isDivisibleBy(R.__, 100), R.not)
+const isDivisibleBy4AndNot100 = R.allPass([isDivisibleBy(R.__, 4), notIsDivisibleBy100])
+export const isLeapYear = R.anyPass([isDivisibleBy4AndNot100, isDivisibleBy400])
 
-export const add5 = R.add(5)
+//plain style:
+// export function isLeapYear (number) {
+//   return (number % 4 === 0 && !(number % 100 === 0) || number % 400 === 0)
+// }
+
+//refactored plain style:
+// function isDivisibleBy (number, divisor) {
+//   return number % divisor === 0
+// }
+// export function isLeapYear (number) {
+//   return (isDivisibleBy(number, 4) && !isDivisibleBy(number, 100) || isDivisibleBy(number, 400))
+// }
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,28 @@
-import {add5} from "../src/app.js";
+import {isLeapYear} from '../src/app.js'
 
-it('Should add 5', () => {
-    expect(add5(3)).toBe(8)
+it('should be divisible by 4', () => {
+    expect(isLeapYear(12)).toBe(true)
 })
-//this is a test
-// and another one
+
+it('should detect non leap year', () => {
+    expect(isLeapYear(1)).toBe(false)
+})
+
+it('should not be divisible by 100', () => {
+    expect(isLeapYear(100)).toBe(false)
+})
+
+it('should be divisible by 400', () => {
+    expect(isLeapYear(400)).toBe(true)
+})
+it.each`
+year | expectedResult
+${4} | ${true}
+${2001} | ${false}
+${1996} | ${true}
+${1900} | ${false}
+${2000} | ${true}
+`
+('Should check if $year is leap year: $expectedResult', ({year, expectedResult}) => {
+    expect(isLeapYear(year)).toBe(expectedResult)
+})
