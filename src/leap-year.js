c2412c49ff6c04383b630b2bb2ad6ee0df3494dd
Robin Schlenker <robin.schlenker@codecentric.de>
2022-07-15 13:00:22 +0200

    Slight refactorings

--- src/leap-year.js
+++ src/leap-year.js
@@ -2,9 +2,9 @@
 import * as R from 'ramda'
 const isDivisibleBy = R.curry(R.pipe(R.modulo, R.equals(0)))
 const isDivisibleBy400 = isDivisibleBy(R.__, 400)
-const notIsDivisibleBy100 = R.pipe(isDivisibleBy(R.__, 100), R.not)
-const isDivisibleBy4AndNot100 = R.allPass([isDivisibleBy(R.__, 4), notIsDivisibleBy100])
-export const isLeapYear = R.anyPass([isDivisibleBy4AndNot100, isDivisibleBy400])
+const notIsDivisibleBy100 = R.complement(isDivisibleBy(R.__, 100))
+const isDivisibleBy4AndNot100 = R.both(isDivisibleBy(R.__, 4), notIsDivisibleBy100)
+export const isLeapYear = R.either(isDivisibleBy4AndNot100, isDivisibleBy400)
 
 //plain style:
 // export function isLeapYear (number) {
